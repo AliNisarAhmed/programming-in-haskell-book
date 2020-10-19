d2549ba08b8158a8a510c36f29391eee0d05d17a
Ali Ahmed <ali.ahmed@punchcard.io>
2020-10-18 19:45:30 -0600

    chapter 10 prob 2

--- part-2/c-11/Main.hs
+++ part-2/c-11/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.Char
 import Data.List
 import System.IO
+import System.Random (randomRIO)
 
 size :: Int
 size = 3
@@ -15,9 +16,6 @@ next :: Player -> Player
 next O = X
 next B = B
 next X = O
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
 
 empty :: Grid
 empty = replicate size (replicate size B)
@@ -67,3 +65,138 @@ showPlayer :: Player -> [String]
 showPlayer O = [" ", " O ", " "]
 showPlayer B = [" ", "   ", " "]
 showPlayer X = [" ", " X ", " "]
+
+valid :: Grid -> Int -> Bool
+valid g i = i >= 0 && i < size ^ 2 && concat g !! i == B
+
+move :: Grid -> Int -> Player -> [Grid]
+move g i p =
+  if valid g i
+    then [chop size (xs ++ [p] ++ ys)]
+    else []
+  where
+    (xs, B : ys) = splitAt i (concat g)
+
+chop :: Int -> [a] -> [[a]]
+chop _ [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+getNat :: String -> IO Int
+getNat prompt = do
+  putStr prompt
+  xs <- getLine
+  if xs /= [] && all isDigit xs
+    then return $ read xs
+    else do
+      putStrLn "ERROR: Invalid Number"
+      getNat prompt
+
+tictactoe :: IO ()
+tictactoe = run empty O
+
+--------
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+goto :: (Int, Int) -> IO ()
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+-----
+
+run :: Grid -> Player -> IO ()
+run g p = do
+  cls
+  goto (1, 1)
+  putGrid g
+  run' g p
+
+run' :: Grid -> Player -> IO ()
+run' g p
+  | wins O g = putStrLn "Player O wins! \n"
+  | wins X g = putStrLn "Player X wins! \n"
+  | full g = putStrLn "It's a draw! \n"
+  | otherwise = do
+    i <- getNat (prompt p)
+    case move g i p of
+      [] -> do
+        putStrLn "ERROR: Invalid move"
+        run' g p
+      [g'] -> run g' (next p)
+
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
+
+data Tree a
+  = Node a [Tree a]
+  deriving (Show)
+
+gametree :: Grid -> Player -> Tree Grid
+gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
+
+moves :: Grid -> Player -> [Grid]
+moves g p
+  | won g = []
+  | full g = []
+  | otherwise = concat [move g i p | i <- [0 .. ((size ^ 2) - 1)]]
+
+prune :: Int -> Tree a -> Tree a
+prune 0 (Node x _) = Node x []
+prune n (Node x ts) = Node x [prune (n - 1) t | t <- ts]
+
+depth :: Int
+depth = 9
+
+minimax :: Tree Grid -> Tree (Grid, Player)
+minimax (Node g [])
+  | wins O g = Node (g, O) []
+  | wins X g = Node (g, X) []
+  | otherwise = Node (g, B) []
+minimax (Node g ts)
+  | turn g == O = Node (g, minimum ps) ts'
+  | turn g == X = Node (g, maximum ps) ts'
+  where
+    ts' = map minimax ts
+    ps = [p | Node (_, p) _ <- ts']
+
+bestmove :: Grid -> Player -> [Grid]
+bestmove g p = [g' | Node (g', p') _ <- ts, p' == best]
+  where
+    tree = prune depth (gametree g p)
+    Node (_, best) ts = minimax tree
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  play empty O
+
+play :: Grid -> Player -> IO ()
+play g p = do
+  cls
+  goto (1, 1)
+  putGrid g
+  play' g p
+
+play' :: Grid -> Player -> IO ()
+play' g p
+  | wins O g = putStrLn "Player O wins! \n"
+  | wins X g = putStrLn "Player X wins! \n"
+  | full g = putStrLn "It's a draw \n"
+  | p == O = do
+    i <- getNat (prompt p)
+    case move g i p of
+      [] -> do
+        putStrLn "ERROR: Invalid move"
+        play' g p
+      [g'] -> play g' (next p)
+  | p == X = do
+    putStr "Player X is thinking..."
+    let moves = bestmove g p
+    n <- randomRIO (0, length moves - 1)
+    play (moves !! n) (next p)
+
+-- 1.
+
+countNodes :: Tree Grid -> Int
+countNodes (Node _ []) = 1
+countNodes (Node _ subtree) = 1 + (sum $ map countNodes subtree)
