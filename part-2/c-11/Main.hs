292e1ab3f10d4bf77caafec8272de26a1c71fe96
Ali Ahmed <ali.ahmed@punchcard.io>
2020-10-22 00:44:38 -0600

    solving 4 c chapter 10

--- part-2/c-11/Main.hs
+++ part-2/c-11/Main.hs
@@ -23,17 +23,23 @@ empty = replicate size (replicate size B)
 full :: Grid -> Bool
 full = all (/= B) . concat
 
-turn :: Grid -> Player
-turn g = if os <= xs then O else X
+turn :: Grid -> Player -> Player
+turn g p =
+  if os < xs
+    then O
+    else
+      if os > xs
+        then X
+        else next p
   where
     os = length (filter (== O) ps)
     xs = length (filter (== X) ps)
     ps = concat g
 
 wins :: Player -> Grid -> Bool
-wins p g = any line (rows ++ cols ++ diags)
+wins p g = any samePlayer (rows ++ cols ++ diags)
   where
-    line = all (== p)
+    samePlayer = all (== p)
     rows = g
     cols = transpose g
     diags = [diag g, diag (map reverse g)]
@@ -61,10 +67,14 @@ showRow = beside . interleave bar . map showPlayer
     beside = foldr1 (zipWith (++))
     bar = replicate 3 "|"
 
+bar1 = [replicate ((size * 4) -1) '-']
+
+bar2 = replicate 3 "|"
+
 showPlayer :: Player -> [String]
-showPlayer O = [" ", " O ", " "]
-showPlayer B = [" ", "   ", " "]
-showPlayer X = [" ", " X ", " "]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
 
 valid :: Grid -> Int -> Bool
 valid g i = i >= 0 && i < size ^ 2 && concat g !! i == B
@@ -103,6 +113,20 @@ goto :: (Int, Int) -> IO ()
 goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 
 -----
+
+g1 = concat $ move empty 4 O
+
+g2 = concat $ move g1 0 X
+
+g3 = concat $ move g2 1 O
+
+g4 = concat $ move g3 2 X
+
+-- g5 = concat $ move g4 2 X
+
+-- g6 = concat $ move g5 1 O
+
+-- g7 = concat $ move g6 6 O
 
 run :: Grid -> Player -> IO ()
 run g p = do
@@ -147,56 +171,81 @@ prune n (Node x ts) = Node x [prune (n - 1) t | t <- ts]
 depth :: Int
 depth = 9
 
-minimax :: Tree Grid -> Tree (Grid, Player)
-minimax (Node g [])
+minimax :: Tree Grid -> Player -> Tree (Grid, Player)
+minimax (Node g []) player
   | wins O g = Node (g, O) []
   | wins X g = Node (g, X) []
   | otherwise = Node (g, B) []
-minimax (Node g ts)
-  | turn g == O = Node (g, minimum ps) ts'
-  | turn g == X = Node (g, maximum ps) ts'
+minimax (Node g ts) player
+  | turn g player == O = Node (g, minimum ps) ts'
+  | turn g player == X = Node (g, maximum ps) ts'
   where
-    ts' = map minimax ts
+    ts' = map (flip minimax $ player) ts
     ps = [p | Node (_, p) _ <- ts']
 
-bestmove :: Grid -> Player -> [Grid]
-bestmove g p = [g' | Node (g', p') _ <- ts, p' == best]
+bestmove :: Grid -> Player -> Grid
+bestmove g p =
+  getGridFromLabeledTree $ minimumBy depthCompare $ [t | t <- ts, getPlayerFromLabeledTree t == best]
   where
     tree = prune depth (gametree g p)
-    Node (_, best) ts = minimax tree
+    Node (_, best) ts = minimax tree p
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  play empty O
+  putStrLn "Do you want to make the first move? (y/n):"
+  res <- getLine
+  case res of
+    "y" -> play empty O
+    "n" -> play (head $ move empty 4 X) O
+    _ -> do
+      putStrLn "Please enter either y or n: "
+      main
 
 play :: Grid -> Player -> IO ()
-play g p = do
-  cls
-  goto (1, 1)
-  putGrid g
-  play' g p
+play g p =
+  do
+    cls
+    goto (1, 1)
+    putGrid g
+    play' g p
 
 play' :: Grid -> Player -> IO ()
 play' g p
-  | wins O g = putStrLn "Player O wins! \n"
-  | wins X g = putStrLn "Player X wins! \n"
-  | full g = putStrLn "It's a draw \n"
+  | wins O g = putStrLn "Player O wins!\n"
+  | wins X g = putStrLn "Player X wins!\n"
+  | full g = putStrLn "It's a draw!\n"
   | p == O = do
     i <- getNat (prompt p)
     case move g i p of
       [] -> do
         putStrLn "ERROR: Invalid move"
         play' g p
-      [g'] -> play g' (next p)
-  | p == X = do
-    putStr "Player X is thinking..."
-    let moves = bestmove g p
-    n <- randomRIO (0, length moves - 1)
-    play (moves !! n) (next p)
+      [g'] ->
+        play g' (next p)
+  | p == X =
+    do
+      putStr "Player X is thinking... "
+      (play $! (bestmove g p)) (next p)
 
 -- 1.
 
 countNodes :: Tree Grid -> Int
 countNodes (Node _ []) = 1
 countNodes (Node _ subtree) = 1 + (sum $ map countNodes subtree)
+
+-- 3.
+
+getPlayerFromLabeledTree :: Tree (Grid, Player) -> Player
+getPlayerFromLabeledTree (Node (_, p) _) = p
+
+getGridFromLabeledTree :: Tree (Grid, Player) -> Grid
+getGridFromLabeledTree (Node (g, _) _) = g
+
+calcDepth :: Tree a -> Int
+calcDepth (Node _ []) = 0
+calcDepth (Node _ ts) = 1 + (maximum $ map calcDepth ts)
+
+depthCompare :: Tree a -> Tree a -> Ordering
+depthCompare (Node _ []) (Node _ []) = EQ
+depthCompare n1 n2 = compare (calcDepth n1) (calcDepth n2)
